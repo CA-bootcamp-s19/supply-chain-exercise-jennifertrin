08a5947b948366fe15fc253c973da4e734feb1b9
jennifertrin <38402540+jennifertrin@users.noreply.github.com>
2020-02-01 06:32:44 -0500

    added ;

--- contracts/SupplyChain.sol
+++ contracts/SupplyChain.sol
@@ -12,7 +12,7 @@ contract SupplyChain {
   address public owner;
 
   /* Add a variable called skuCount to track the most recent sku # */
-  uint public skuCount
+  uint public skuCount;
 
   /* Add a line that creates a public mapping that maps the SKU (a number) to an Item.
      Call this mappings items
